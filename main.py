1dcc002d53bde32ee1852fe7e4ed607d2503ceed
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2024-08-02 15:26:35 -0500

    example to save one page

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ credentials = AuthCredentials(user_file="me")
 site = WikiggClient('gg', credentials=credentials)
 summary = 'Bot edit'
 
+site.save_title(site.client.pages['User:RheingoldRiver/test2'], 'hello world')
 
-class TemplateModifier(TemplateModifierBase):
-    def update_template(self, template: Template):
-        return
-
-
-TemplateModifier(site, 'TEMPLATEYOUCAREABOUT',
-                 summary=summary).run()
+# class TemplateModifier(TemplateModifierBase):
+#     def update_template(self, template: Template):
+#         return
+#
+#
+# TemplateModifier(site, 'TEMPLATEYOUCAREABOUT',
+#                  summary=summary).run()
