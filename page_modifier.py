bf5a8063fb56f8bbcfc39a7c9442b390942cd1b3
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2024-08-02 16:30:36 -0500

    add page modifier example

--- page_modifier.py
+++ page_modifier.py
@@ -1,15 +1,24 @@
-from mwcleric import WikiggClient
+from mwcleric.wikigg_client import WikiggClient
 from mwcleric.auth_credentials import AuthCredentials
-from mwcleric.template_modifier import TemplateModifierBase
+from mwcleric.page_modifier import PageModifierBase
 from mwparserfromhell.nodes import Template
 
 credentials = AuthCredentials(user_file="me")
 site = WikiggClient('gg', credentials=credentials)
-summary = 'Updating parameters `materials` and `amounts` to current standard'
+summary = 'Bot edit'
 
 
-class TemplateModifier(TemplateModifierBase):
-    def update_template(self, template: Template):
+class PageModifier(PageModifierBase):
+    def update_wikitext(self, wikitext):
+        for template in wikitext.filter_templates(recursive=True):
+            name = template.name
+            if name.matches('Infobox Building'):
+                self.update_infobox_building(template)
+            elif name.matches('Not Infobox Building'):
+                self.update_not_infobox_building(template)
+
+    def update_infobox_building(self, template: Template):
+        # do stuff here
         if template.has('material') and template.has('material1'):
             raise ValueError
         if template.has('amount') and template.has('amount1'):
@@ -43,6 +52,12 @@ class TemplateModifier(TemplateModifierBase):
         text = ', '.join(items)
         template.add(plural, text)
 
+    def update_not_infobox_building(self, template: Template):
+        # do stuff here
+        if not template.has('param'):
+            return
+        template.get('param').value = 'you have been changed'
 
-TemplateModifier(site, 'Infobox Building',
-                 summary=summary).run()
+
+PageModifier(site, site.pages_using('Infobox Building'),
+             summary=summary).run()
